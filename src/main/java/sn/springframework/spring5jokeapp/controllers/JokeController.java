66d3156b3d5c8cc394ee4dc53d5127eccb7e36ae
sngom1995 <ngomsamba75@gmail.com>
2023-02-11 22:11:42 +0000

    creating controller layer for joke

---
+++ src/main/java/sn/springframework/spring5jokeapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package sn.springframework.spring5jokeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sn.springframework.spring5jokeapp.services.JokeService;
+
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+            model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
